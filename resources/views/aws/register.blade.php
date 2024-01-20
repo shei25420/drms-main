54fe5237f0069ceabaa1af1bb16d5b82ffc256e5
Only1allan <kariukiallan850@gmail.com>
2024-01-21 01:55:04 +0300

    Aws register js

--- resources/views/aws/register.blade.php
+++ resources/views/aws/register.blade.php
@@ -1,29 +1,49 @@
 @extends('layouts.auth')
 @section('tab-title')
-				{{ __('Verify') }}
+    {{ __('Verify') }}
 @endsection
+@push('script-page')
+	<script>
+		const form = document.getElementById('aws-register-form');
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			const params = new Proxy(new URLSearchParams(window.location.search), {
+  				get: (searchParams, prop) => searchParams.get(prop),
+			});
+
+			var hiddenInput = document.createElement('input');
+			hiddenInput.setAttribute('type', 'hidden');
+			hiddenInput.setAttribute('name', 'clientId');
+			hiddenInput.setAttribute('value', params.customer_id);
+
+			e.target.addChild(hiddenInput);
+			e.target.submit();
+		});
+	</script>
+@endpush
 @section('content')
-				<div class="codex-authbox">
-								<div class="auth-header">
-												<div class="auth-icon"><i class="fa fa-unlock-alt"></i></div>
-												<h3>{{ __('Verify Your Email Address') }}</h3>
-												@if (session('resent'))
-																<div class="alert alert-success" role="alert">
-																				{{ __('An email has been sent with your account deatils.') }}
-																</div>
-												@endif
-												<form action="/aws/register" method="POST">
-													@csrf
-																<div class="form-group">
-																				<label for="email">Email Address</label>
-																				<input type="email" class="form-control" placeholder="Please enter your email address">
-																</div>
-																<div class="form-group">
-																				<button type="submit" class="btn btn-primary btn-sm">Create Account</button>
-																</div>
-												</form>
-								</div>
-								<div class="auth-footer">
-								</div>
-				</div>
+    <div class="codex-authbox">
+        <div class="auth-header">
+            <div class="auth-icon"><i class="fa fa-unlock-alt"></i></div>
+            <h3>{{ __('Verify Your Email Address') }}</h3>
+            @if (session('resent'))
+                <div class="alert alert-success" role="alert">
+                    {{ __('An email has been sent with your account deatils.') }}
+                </div>
+            @endif
+            <form id="aws-register-form">
+                @csrf
+                <div class="form-group">
+                    <label for="email">Email Address</label>
+                    <input type="email" class="form-control" placeholder="Please enter your email address">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-sm">Create Account</button>
+                </div>
+            </form>
+        </div>
+        <div class="auth-footer">
+        </div>
+    </div>
 @endsection
