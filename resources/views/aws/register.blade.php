7cd2bc4b2793711cc2201534b310edd1cb4a1d8f
Only1allan <kariukiallan850@gmail.com>
2024-01-21 02:24:57 +0300

    added email name aws reg form

--- resources/views/aws/register.blade.php
+++ resources/views/aws/register.blade.php
@@ -36,7 +36,7 @@
                 @csrf
                 <div class="form-group">
                     <label for="email">Email Address</label>
-                    <input type="email" class="form-control" placeholder="Please enter your email address">
+                    <input type="email" class="form-control" name="email" placeholder="Please enter your email address">
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary btn-sm">Create Account</button>
