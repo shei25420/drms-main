a5f14391870c097cb8009f0a809bf5dc2cb2f74a
Only1allan <kariukiallan850@gmail.com>
2024-02-09 23:11:33 +0300

    email message fix

--- resources/views/email/aws_customer_created.blade.php
+++ resources/views/email/aws_customer_created.blade.php
@@ -4,8 +4,25 @@
     <title>{{env('APP_NAME')}}</title>
 </head>
 <body>
-<h1>Thank you for joining the family. Below is the password to your new account</h1>
-<p>Password: {{ $password }}</p>
-<p>{{__('Thank you')}}</p>
+<p>Dear {{$name}},
+
+    Welcome to {{env('APP_NAME')}} Vault! We're thrilled to have you on board and excited for the journey ahead. Thank you for choosing our platform to manage your documents.
+    
+    Here are a few steps to help you get started with {{env('APP_NAME')}}:
+    
+        Login to Your Account: Head over to our website https://drmsvault.com/login and log in to your account using the credentials you provided during sign-up.
+    
+        Explore Your Dashboard: Once logged in, you'll find yourself in your personalized dashboard. Take some time to explore the features and functionalities available to you.
+    
+        Support and Assistance: Should you have any questions, encounter any issues, or simply need assistance, don't hesitate to reach out to our support team via email. We're here to help you succeed.
+    
+    We're committed to providing you with an exceptional experience, and we're confident that {{env('APP_NAME')}} vault will empower you to manage, retrieve, and store your documents.
+    
+    Once again, welcome aboard! We look forward to seeing your success with {{env('APP_NAME')}}.
+    
+    Best Regards,
+    
+    Support team at {{env('APP_NAME')}}</p>
 </body>
 </html>
+
