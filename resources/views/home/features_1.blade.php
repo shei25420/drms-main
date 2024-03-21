5ea7d4e71d55f610d57e8ded259b9f5bb4cdb1bb
Allan Kariuki <90037613+Only1allan@users.noreply.github.com>
2024-03-21 23:32:38 +0300

    ffff

--- resources/views/home/features_1.blade.php
+++ resources/views/home/features_1.blade.php
@@ -38,7 +38,7 @@
                     </div>
                     <div class="col-lg-12 col-md-6">
                         <div class="demo-grid">
-                            <div class="img-wrap"><img class="img-fluid" src="assets/images/home/User Logged History-01.png" alt=""></div>
+                            <div class="img-wrap"><img class="img-fluid" src="assets/images/home/User Logged History-01.png" alt="user log demo"></div>
                             <div class="demo-detail">
                                 <h3 class="text-white">User Logged History</h3>
                             </div>
