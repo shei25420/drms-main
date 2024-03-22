050156f5c19a673a4dbcf1bd8dc2b8a93b3817dd
m0th3rch1p <njagibret@gmail.com>
2024-03-22 06:14:42 +0300

    even more ui | Color

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -27,7 +27,7 @@
 																																<li class="menu-item"><a href="#">{{ __('Home') }}</a></li>
 																																<li class="menu-item"><a href="#features">{{ __('Features') }}</a></li>
 																																<li class="menu-item"><a href="#pricing">{{ __('Pricing') }}</a></li>
-																																<li class="menu-item">
+																																<li class="menu-item" style="color: #fffff">
 																																				<!-- login and register routers incomplete -->
 																																				<a class="btn btn-primary me-2" href="{{ route('login') }}">{{ __('Login') }} </a>
 																																				<a class="btn btn-primary" href="{{ route('register') }}">{{ __('Register') }}
